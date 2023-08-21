16a82a4fa8b90d2472e7090323bfd0f28741f56f
Sid-Dhawan-technocrat <sdhawan127@gmail.com>
2023-08-21 09:56:01 +0530

    api functionality added

--- script.js
+++ script.js
@@ -96,21 +96,33 @@ function autocomplete(inp, arr) {
   }
   
   /*An array containing all the country names in the world:*/
-  var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+  var countries= ["Amsterdam","Ambala","Chennai","Delhi","Dhaka","Lucknow","Pathankot","Prayagraj","Panipat","Patna","Shimla","San Diego"];
   
   /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), countries);
+let x = document.getElementById("myInput");
+      let y = document.getElementById("location");
+      let z = document.getElementById("weather");
 
 function getFormValue(e){
     e.preventDefault();
     const getCityName=document.getElementById("myInput").value;
     console.log(getCityName);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${getCityName}&appid=3378232ecde9e975edb427f14dd39fc8
+    `;
+    //api calling
+    fetch(url, { method: "GET" })
+      //return promise
+      .then((data) => data.json())
+      .then((res) => {
+        console.log("Weather data", res);})
 
-}
+      y
 
-    //   let x = document.getElementById("out");
-    //   let y = document.getElementById("location");
-    //   let z = document.getElementById("weather");
+
+      }
+
+    
     //   function geolocation() {
     //     if (navigator.geolocation) {
     //       navigator.geolocation.getCurrentPosition(showPosition); //Callback function
@@ -127,13 +139,7 @@ function getFormValue(e){
     //     x.innerText = `Lat is ${lat} and Long is ${long}`;
     //     //to generate api key and call => https://openweathermap.org/current
 
-    //     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=019c012e8e9bcdc7c84a82c0bf685c83`;
-    //     //api calling
-    //     fetch(url, { method: "GET" })
-    //       //return promise
-    //       .then((data) => data.json())
-    //       .then((res) => {
-    //         console.log("Weather data", res);
+         
     //         y.innerText = `Temp of the day is ${res.main.temp}`;
     //         z.innerText = `City name is ${res.name}`;
     //         // console.log("Weather data", res.name);
